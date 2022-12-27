b1bf375043fcdbd9a5ecd17f5ba8966ab501fa06
FilippoTi <filippo.tiberio@libero.it>
2022-12-27 11:15:06 +0100

    refactoring coordinates

--- src/coordinate/coordinate.cpp
+++ src/coordinate/coordinate.cpp
@@ -3,7 +3,19 @@
 //
 #include "./../../include/coordinate/coordinate.h"
 
-std::pair<int, int> Coordinate::coordinate_to_index(Coordinate c) {
-//TODO!
-    return std::make_pair(c.row() - 'A', c.col());
+std::pair<int, int> Coordinate::coordinates_to_indexes(Coordinate c) {
+    return std::make_pair(c.row(), c.col());
+}
+
+std::pair<char, int> Coordinate::indexes_to_coordinates(Coordinate c) {
+
+    return std::make_pair(customRow_to_char(c.row()), c.col());
+}
+
+std::ostream &operator<<(std::ostream &os, const Coordinate &coordinate) {
+    std::pair<char, int> customCoord = Coordinate::indexes_to_coordinates(coordinate);
+
+    os << "Coordinate: (" << customCoord.first << ", " << customCoord.second << "), [" << coordinate.row() << ", "
+       << coordinate.col() << "] " << std::endl;
+    return os;
 }
