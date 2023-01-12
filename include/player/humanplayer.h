bb38c3220dcf876c34644e852e40303228542b51
Matteo Galiazzo <mattegaliz@gmail.com>
2023-01-13 00:21:37 +0100

    added color.h, changed print boards inline ostream

--- include/player/humanplayer.h
+++ include/player/humanplayer.h
@@ -3,13 +3,14 @@
 
 #include <sstream>
 
+#include "./../utils/colors.h"
+#include "player.h"
+
 #if defined(__linux__) || defined(__APPLE__)
 
 #else
 #include <windows.h>
 #endif
-
-#include "player.h"
 
 class Humanplayer : public Player {
    public:
--- src/ship/submarine.cpp
+++ src/ship/submarine.cpp
@@ -1,11 +1,13 @@
 #include "./../../include/ship/submarine.h"
+// ???
+#include "./../../include/board/attackboard.h"
+#include "./../../include/board/defenseboard.h"
 
 std::ostream &operator<<(std::ostream &os, const Submarine &battleship) {
     std::vector<bool> cells = battleship.cells();
 
-    os << "Submarine with center in " << battleship.center()
-       << "and direction: " << battleship.direction() << ", has "
-       << battleship.armor() << "/" << Submarine::LENGTH << " armor: [";
+    os << "Submarine with center in " << battleship.center() << "and direction: " << battleship.direction()
+       << ", has " << battleship.armor() << "/" << Submarine::LENGTH << " armor: [";
 
     for (bool cell : cells) {
         os << (cell ? Submarine::CHARACTER : (char)(Submarine::CHARACTER + 32));
@@ -14,4 +16,24 @@ std::ostream &operator<<(std::ostream &os, const Submarine &battleship) {
     os << "]";
 
     return os;
+}
+
+bool Submarine::action(Coordinate dest, Defenseboard &opponent, Attackboard &self) {
+    if (!dest.is_valid()) throw Coordinate::INVALID_COORDINATE{};
+    // scan destination, but i have to TODO: scan surrounding 5x5
+    // go up by 2, left by 2
+    // start 5x5 nested for loop with reveal
+    Coordinate to_check = dest;
+
+    to_check.set_col(to_check.col() - 1);
+    to_check.set_col(to_check.col() - 1);
+
+    to_check.set_row(to_check.row() - 1);
+    to_check.set_row(to_check.row() - 1);
+
+    if (!opponent.is_occupied(dest)) {
+        return true;
+    }
+    self.reveal(dest);
+    return true;
 }
